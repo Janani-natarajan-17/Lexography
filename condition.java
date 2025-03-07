91ffafdd2ebd3d61ff94960131e523e1eba4a87e
Janani N <natarajanjanani2004@gmail.com>
2025-03-07 10:47:06 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,16 @@
+package janani;
+import java.util.*;
+
+public class condition {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = a%10;
+	if(b==3 || b==8)
+	{
+		System.out.println("lucky winner");
+	}
+
+
+}
+}
